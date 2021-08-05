22c8f0e86f260196c338f7688e8b564327a1a052
Chris Reid <c.reid@wellcome.org>
2021-08-05 15:21:13 +0100

    8690 docs, 8689 docs: tweaking Colours and Spacing storybook presentation

--- src/stories/Spacing.stories.tsx
+++ src/stories/Spacing.stories.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import kebabCase from 'lodash/fp/kebabCase';
+
 type SizeProps = {
   [key: string]: {
     [key: string]: string;
@@ -8,31 +10,31 @@ type SizeProps = {
 
 export const SpacingBlocks = () => {
   const sizes: SizeProps = {
-    xl: {
+    SpaceXl: {
       color: '--color-green-50',
       mqBase: '32',
       mqSmall: '48',
       mqMedium: '64'
     },
-    lg: {
+    SpaceLg: {
       color: '--color-blue-50',
       mqBase: '16',
       mqSmall: '24',
       mqMedium: '32'
     },
-    md: {
+    SpaceMd: {
       color: '--color-orange-50',
       mqBase: '8',
       mqSmall: '12',
       mqMedium: '16'
     },
-    sm: {
+    SpaceSm: {
       color: '--color-red-50',
       mqBase: '6',
       mqSmall: '8',
       mqMedium: '8'
     },
-    xs: {
+    SpaceXs: {
       color: '--color-amber-40',
       mqBase: '4',
       mqSmall: '4',
@@ -45,7 +47,9 @@ export const SpacingBlocks = () => {
       <caption>Standard space units and their sizes across breakpoints</caption>
       <thead>
         <tr>
-          <th scope="col">Space unit</th>
+          <th scope="col">Token</th>
+          <th scope="col">CSS variable</th>
+          <th scope="col">Preview</th>
           <th scope="col">Default size, small screens up to 767 pixels</th>
           <th scope="col">Medium screens from 768 up to 1023 pixels</th>
           <th scope="col">Large screens 1024 pixels wide and above</th>
@@ -53,9 +57,12 @@ export const SpacingBlocks = () => {
       </thead>
       <tbody>
         {Object.entries(sizes).map(([key, value]) => {
-          const size = `--space-${key}`;
+          const size = `--${kebabCase(key)}`;
+
           return (
             <tr key={size}>
+              <td>{key}</td>
+              <td className="sb-variable">{size}</td>
               <td>
                 <figure>
                   <div
@@ -66,14 +73,7 @@ export const SpacingBlocks = () => {
                       width: `var(${size})`
                     }}
                   />
-                  <figcaption className="sb-swatch__name">
-                    <dl>
-                      <dt className="u-visually-hidden">Space unit name</dt>
-                      <dd>{key.toUpperCase()}</dd>
-                      <dt className="u-visually-hidden">CSS variable</dt>
-                      <dd className="sb-variable">{size}</dd>
-                    </dl>
-                  </figcaption>
+                  <figcaption className="u-visually-hidden">Coloured square indicating the size of {size}</figcaption>
                 </figure>
               </td>
               <td>{value.mqBase} pixels</td>
---
+++ src/types/index.d.ts
@@ -0,0 +1,3 @@
+type MappedTokensProps = {
+  [key: string]: string;
+};
