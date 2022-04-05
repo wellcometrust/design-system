a611ed0182b7976f393695796cbf0b0bfcac3c11
Ewelina Skibinska <E.Skibinska@wellcome.ac.uk>
2022-04-05 11:35:12 +0100

    9583 task: updates IconActionMute fill colour

--- src/components/Icon/components/IconActionMute.tsx
+++ src/components/Icon/components/IconActionMute.tsx
@@ -9,7 +9,7 @@ const IconActionMute = ({ viewBox = '0 0 24 24' }: { viewBox?: string }) => (
       </g>
       <path
         d="M16 4a8 8 0 1 1 0 16l-.117-.007a1 1 0 0 1 0-1.986L16 18a6 6 0 1 0 0-12l-.117-.007a1 1 0 0 1 0-1.986L16 4Zm0 4a4 4 0 1 1 0 8l-.117-.007A1 1 0 0 1 16 14a2 2 0 1 0 0-4l-.117-.007A1 1 0 0 1 16 8Z"
-        fill="#000"
+        fill="currentColor"
       />
       <rect
         fill="currentColor"
