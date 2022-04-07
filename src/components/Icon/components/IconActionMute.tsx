a35795ab1b33f7c5f019ff44cf20ae34d213a383
Ewelina Skibinska <E.Skibinska@wellcome.ac.uk>
2022-04-07 15:14:53 +0100

    9583 task: changes to icons after using svg-batch-transforms

--- src/components/Icon/components/IconActionMute.tsx
+++ src/components/Icon/components/IconActionMute.tsx
@@ -5,10 +5,10 @@ const IconActionMute = ({ viewBox = '0 0 24 24' }: { viewBox?: string }) => (
     <g fill="none" fillRule="evenodd">
       <g transform="translate(-1 4)" fill="currentColor">
         <rect x="1" y="4" width="9" height="8" rx="1" />
-        <path d="m2.067 8.8 9.333 7A1 1 0 0 0 13 15V1a1 1 0 0 0-1.6-.8l-9.333 7a1 1 0 0 0 0 1.6Z" />
+        <path d="m2.067 8.8 9.333 7A1 1 0 0 0 13 15V1a1 1 0 0 0-1.6-.8l-9.333 7a1 1 0 0 0 0 1.6z" />
       </g>
       <path
-        d="M16 4a8 8 0 1 1 0 16l-.117-.007a1 1 0 0 1 0-1.986L16 18a6 6 0 1 0 0-12l-.117-.007a1 1 0 0 1 0-1.986L16 4Zm0 4a4 4 0 1 1 0 8l-.117-.007A1 1 0 0 1 16 14a2 2 0 1 0 0-4l-.117-.007A1 1 0 0 1 16 8Z"
+        d="M16 4a8 8 0 1 1 0 16l-.117-.007a1 1 0 0 1 0-1.986L16 18a6 6 0 1 0 0-12l-.117-.007a1 1 0 0 1 0-1.986L16 4zm0 4a4 4 0 1 1 0 8l-.117-.007A1 1 0 0 1 16 14a2 2 0 1 0 0-4l-.117-.007A1 1 0 0 1 16 8z"
         fill="currentColor"
       />
       <rect
--- src/components/Icon/components/IconActionPause.tsx
+++ src/components/Icon/components/IconActionPause.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const IconActionPause = ({ viewBox = '0 0 24 24' }: { viewBox?: string }) => (
   <svg viewBox={viewBox}>
     <path
-      d="M6.857 1.143h2.714a1 1 0 0 1 1 1v20a1 1 0 0 1-1 1H6.857a1 1 0 0 1-1-1v-20a1 1 0 0 1 1-1Zm7.857 0h2.715a1 1 0 0 1 1 1v20a1 1 0 0 1-1 1h-2.715a1 1 0 0 1-1-1v-20a1 1 0 0 1 1-1Z"
+      d="M6.857 1.143h2.714a1 1 0 0 1 1 1v20a1 1 0 0 1-1 1H6.857a1 1 0 0 1-1-1v-20a1 1 0 0 1 1-1zm7.857 0h2.715a1 1 0 0 1 1 1v20a1 1 0 0 1-1 1h-2.715a1 1 0 0 1-1-1v-20a1 1 0 0 1 1-1z"
       fill="currentColor"
       fillRule="evenodd"
     />
